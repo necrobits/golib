46d2c8e72339ef8483adb86f683cc2098138698f
etuchnap28 <minh.le.public@gmail.com>
2023-12-08 15:13:32 +0100

    changed key value store interface

--- kvstore/store.go
+++ kvstore/store.go
@@ -5,6 +5,7 @@ type Data interface{}
 type KvStore interface {
 	Get(key string) (Data, error)
 	Set(key string, value Data) error
+	SetMany(data map[string]Data) error
 	Delete(key string) error
-	Transaction(func(KvStore) error) error
+	DeleteMany(keys []string) error
 }
