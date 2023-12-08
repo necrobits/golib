862a77ac4fe4ea251519adc8eeddaad6cb7d75c6
etuchnap28 <minh.le.public@gmail.com>
2023-12-08 18:29:22 +0100

    added getAll

--- kvstore/store.go
+++ kvstore/store.go
@@ -3,6 +3,7 @@ package kvstore
 type Data interface{}
 
 type KvStore interface {
+	GetAll() (map[string]Data, error)
 	Get(key string) (Data, error)
 	Set(key string, value Data) error
 	SetMany(data map[string]Data) error
