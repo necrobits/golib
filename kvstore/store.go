e1032393e30bf9fe08beb04c151eac6919382b17
etuchnap28 <minh.le.public@gmail.com>
2024-02-01 01:44:37 +0100

    Updated Key-value store interface

--- kvstore/store.go
+++ kvstore/store.go
@@ -1,12 +1,12 @@
 package kvstore
 
+import "context"
+
 type Data interface{}
 
 type KvStore interface {
-	GetAll() (map[string]Data, error)
-	Get(key string) (Data, error)
-	Set(key string, value Data) error
-	SetMany(data map[string]Data) error
-	Delete(key string) error
-	DeleteMany(keys []string) error
+	Get(ctx context.Context, key string) (Data, error)
+	Set(ctx context.Context, key string, value Data) error
+	Delete(ctx context.Context, key string) error
+	Transaction(ctx context.Context, fn func(tx KvStore) error) error
 }
